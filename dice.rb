4a418a0e351e2f2bd98f5885a099e7ec02721ef9
ChangSonia <79300461+ChangSonia@users.noreply.github.com>
2025-01-27 15:49:11 +0000

    erb method

--- dice.rb
+++ dice.rb
@@ -15,14 +15,12 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 # home page
 get ("/dice/home") do 
-  "<h1>Dice Roll</h1>
-  <ul>
-  <li><a href = https://special-giggle-p697477664jh9r56-4567.app.github.dev/dice/2/6>Roll two 6-sided dice</a></li>
-  <li><a href = https://special-giggle-p697477664jh9r56-4567.app.github.dev/dice/2/10>Roll two 10-sided dice</a></li>
-  <li><a href = https://special-giggle-p697477664jh9r56-4567.app.github.dev/dice/1/20>Roll one 20-sided dice</a></li>
-
-  </ul>"
+  erb(:elephant)
 end
+
+  # The argument to the erb method must be a Symbol containing the name of a file within the views/ folder
+  # We don’t have to include the .erb part of the filename because it’s assumed that all view template names will end in .erb.
+
 
 
 
